ad7571b4242fb107d14c9b6bf3c70354b69f01bf
Pop-Ana <ana.pop@htley.at>
2025-01-12 15:32:39 +0100

    forgot to remove a code-line

--- myportal/workshopportal/workshops/urls.py
+++ myportal/workshopportal/workshops/urls.py
@@ -1,7 +1,6 @@
 # filepath: /c:/WorkshopPortal_local/myportal/workshopportal/workshops/urls.py
 from django.urls import path, include
 from . import views
-from .views import home, microsoft_login, microsoft_callback
 
 urlpatterns = [
     path('workshops/', views.list_workshops, name='workshops'),
